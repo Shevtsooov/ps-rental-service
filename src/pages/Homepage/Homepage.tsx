69d13af0f8d269ff664fad341f02fe6ece6c1aea
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-10-13 17:10:38 +0300

    finished H games

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -8,21 +8,58 @@ import games from '../../data/games.json'
 import { Aaa } from '../../components/111/111';
 
 export const Homepage: React.FC = () => {
+  let categories: string[] = [];
+
+  const addd = games.map(game => game.category.map(cat =>
+    categories.push(cat)
+  ))
+
+  const catSet = new Set(categories);
+
+  const fff = Array.from(catSet)
+
+  const allTheCategories = [
+    'Пригоди',
+    'Платформери',
+    'Екшн',
+    'Бійки',
+    'Кооперативні',
+    'Симулятори',
+    'Головоломки',
+    'Шутери',
+    'Космос',
+    'Рольові',
+    'Сімейні',
+    'Стратегії',
+    'Аркади',
+    'Спортивні',
+    'Відкритий світ',
+    'Гонки',
+    'Музика',
+    'Риболовля',
+    'Дитячі',
+    'Жахи',
+    'Настільні',
+    'Детективи',
+    'Виживання',
+    "Для нього та для неї"
+  ]
 
   return (
     <div className="homepage">
       <Slider />
 
-      {/* <Aaa />
+      {/* <Aaa /> */}
 
-        <div>
-          <ul>
-            {games.map(game => (
-              // <li>{`type nul > ${game.gameId}.jpg`}</li>
-              <img src={`${game.poster}`} alt="" className='aaa'/>
-            ))}
-          </ul>
-        </div> */}
+      {/* <div>
+        <ul>
+          {fff.map(game => (
+            <li>{game}</li>
+            // <img src={`${game.poster}`} alt="" className='aaa'/>
+          ))}
+        </ul>
+      </div> */}
+
       <BookPS />
 
       <PopularGames />

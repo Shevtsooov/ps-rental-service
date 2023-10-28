e4ae3b11dc51b65b03a699502edfe458cf5b1d19
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-10-28 14:44:37 +0300

    added basic markup to the login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginPage.scss';
 
 
 export const LoginPage: React.FC = () => {
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [fieldType, setFieldType] = useState('password');
 
   return (
     <div className="loginPage">
-      Логін
+      <h1 className="loginPage__title">
+        Авторизація
+      </h1>
+
+      <div className="loginPage__form">
+        <div className="loginPage__form_field">
+          <input
+            className="loginPage__input"
+            placeholder='Email'
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="loginPage__form_field">
+          <input
+            className="loginPage__input loginPage__input--spaced"
+            placeholder='Пароль'
+            type={fieldType}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {password !== '' && (
+            <button
+              className="loginPage__showPassword"
+              onMouseDown={() => setFieldType('text')}
+              onMouseUp={() => setFieldType('password')}
+            />
+          )}
+
+          <button className="loginPage__lostPassword">
+            відновити пароль
+          </button>
+        </div>
+
+        <div className="loginPage__form_actions">
+          <button
+            className="loginPage__form_button loginPage__form_button--registration"
+          >
+            Реєстрація
+          </button>
+
+          <button
+            className="loginPage__form_button loginPage__form_button--login"
+          >
+            Увійти
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
